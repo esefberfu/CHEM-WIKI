7178ec6ca2738f576e119218c54ff6c2f74244a7
Anil Gurses <anilgurses98@gmail.com>
2022-09-02 14:45:36 -0400

    Reallocating the logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CHEM'
+copyright = '2022'
+author = 'Anıl Gürses'
 
 release = '0.1'
 version = '0.1.0'
