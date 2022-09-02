589eb48c7a9c51601f09b6693b5de20a97a22115
Anil Gurses <anilgurses98@gmail.com>
2022-09-02 14:43:48 -0400

    Reallocating the logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_logo = "chem_icon.png"
+html_logo = "_static/images/chem_icon.png"
 
 
 # -- Options for EPUB output
